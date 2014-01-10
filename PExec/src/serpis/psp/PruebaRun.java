4c296c1aa8d84061eecc8ba540b00c1b330f6a33
ivives <ivives75@gmail.com>
2014-01-10 11:24:43 +0100

    añado PExec

---
+++ PExec/src/serpis/psp/PruebaRun.java
@@ -0,0 +1,18 @@
+package serpis.psp;
+
+public class PruebaRun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try{
+			
+		Process  aplicacion = Runtime.getRuntime().exec ("/usr/bin/chromium.browser"); 
+		
+		}
+        catch(Exception e){}
+
+	}
+
+}
