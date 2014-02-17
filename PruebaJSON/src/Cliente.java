d929791899af4cf51d7ebe509449723f15e8c78c
ivives <ivives75@gmail.com>
2014-02-17 19:01:15 +0100

    añado pruebaJSon y PJsonPHP conexion a db con JSon

---
+++ PruebaJSON/src/Cliente.java
@@ -0,0 +1,31 @@
+
+public class Cliente {
+
+	 private String dirección;
+	    private String nombre;
+	    private String telefono;
+	 
+	    public String getDirección() {
+	        return dirección;
+	    }
+	 
+	    public void setDirección(String dirección) {
+	        this.dirección = dirección;
+	    }
+	 
+	    public String getNombre() {
+	        return nombre;
+	    }
+	 
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+	 
+	    public String getTelefono() {
+	        return telefono;
+	    }
+	 
+	    public void setTelefono(String telefono) {
+	        this.telefono = telefono;
+	    }
+	}
